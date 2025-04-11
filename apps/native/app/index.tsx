bc843e62d89948bf1c16a581aca85c90d3f91d1c
Melody Universe <melody@computost.com>
2025-04-11 14:06:50 -0700

    display weather in mobile app

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from "react-native";
+import { Api } from "api-client";
+import { ReactNode, useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +11,59 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <WeatherForecast />
     </View>
   );
 }
+
+function WeatherForecast(): ReactNode {
+  const forecast = useWeatherForecast();
+
+  if (!forecast) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <FlatList
+      data={forecast}
+      renderItem={(info) => (
+        <View>
+          <Text>{info.item.date?.toLocaleDateString()}</Text>
+          <Text>{info.item.summary}</Text>
+          <Text>{info.item.temperature}</Text>
+        </View>
+      )}
+      keyExtractor={(_, i) => i.toString()}
+    />
+  );
+}
+
+function useWeatherForecast(): Forecast[] | null {
+  const [forecasts, setForecasts] = useState<Forecast[] | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const api = new Api({
+        baseUrl: "http://localhost:8080",
+      });
+      const response = await api.weatherforecast.getWeatherForecast({
+        mode: "cors",
+      });
+      setForecasts(
+        response.data.map((obj) => ({
+          date: obj.date ? new Date(Date.parse(obj.date)) : undefined,
+          summary: obj.summary ?? undefined,
+          temperature: obj.temperatureF ?? undefined,
+        }))
+      );
+    })();
+  }, []);
+
+  return forecasts;
+}
+
+type Forecast = {
+  date?: Date;
+  summary?: string;
+  temperature?: number;
+};
